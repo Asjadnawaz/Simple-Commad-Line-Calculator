5225a6942d3687c1d4b8f3d51ab4a8968e5346a2
Asjad Nawaz Khan <asjadnawaz2002@gmail.com>
2024-03-16 16:30:16 +0500

    Commit

---
+++ index.js
@@ -0,0 +1,35 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+let answer = await inquirer.prompt([{
+        message: "Enter your First Value",
+        type: "number",
+        name: "FirstNumber"
+    },
+    {
+        message: "Enter your Second Number",
+        type: "number",
+        name: "SecondNumber"
+    },
+    {
+        message: "Please Select one of the Operators to start Operations",
+        type: "list",
+        name: "Operators",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division", "Modulo"]
+    }
+]);
+// Conditional Statements
+if (answer.Operators === "Addition") {
+    console.log("Your answer is" + " " + chalk.greenBright.bold(answer.FirstNumber + answer.SecondNumber));
+}
+if (answer.Operators === "Subtraction") {
+    console.log("Your answer is" + " " + chalk.greenBright.bold(answer.FirstNumber - answer.SecondNumber));
+}
+if (answer.Operators === "Multiplication") {
+    console.log("Your answer is" + " " + chalk.greenBright.bold(answer.FirstNumber * answer.SecondNumber));
+}
+if (answer.Operators === "Divison") {
+    console.log("Your answer is" + " " + chalk.greenBright.bold(answer.FirstNumber / answer.SecondNumber));
+}
+if (answer.Operators === "Modulo") {
+    console.log("Your answer is" + " " + chalk.greenBright.bold(answer.FirstNumber % answer.SecondNumber));
+}
